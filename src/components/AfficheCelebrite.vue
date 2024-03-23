2e511156f604bcc3d6d581b41ba696b88ce97bf6
Wali-Youssef <117752075+Wali-Youssef@users.noreply.github.com>
2024-03-23 15:09:31 +0100

    enfin

--- src/components/AfficheCelebrite.vue
+++ src/components/AfficheCelebrite.vue
@@ -2,19 +2,20 @@
 import { useRoute } from 'vue-router/auto';
 import { defineProps } from 'vue';
 import { supabase } from '@/supabase';
-const route = useRoute('/celebrite/[id]');
 import type { Database, Tables } from '@/supabase-types';
 defineProps <Database["public"]["Tables"]["Films"]["Row"] & {Celebrite:Tables<'Celebrite'>[]} & {Genre:Tables<'Genre'>[]} & {Saga:Tables<'Saga'>[]}>()
 </script>
 
 <template>
     <div>
-        <h2>{{ nom_film }}</h2>
-        <p>{{ description_film }}</p>
-        <p>{{ note_film }}</p>
-        <p>{{ date_film }}</p>
-        <p>{{ temps_film }}</p>
+
+
         <p v-for="uneCelbrite in Celebrite">{{ uneCelbrite.nom_celebrite }}</p>
         <p v-for="uneCelbrite in Celebrite">{{ uneCelbrite.prenom_celebrite }}</p>
+        <p v-for="uneCelbrite in Celebrite">{{ uneCelbrite.date_de_naissance }}</p>
+        <p v-for="uneCelbrite in Celebrite">{{ uneCelbrite.date_deces }}</p>
+        <p v-for="uneCelbrite in Celebrite">{{ uneCelbrite.description_celebrite }}</p>
+        <p v-for="uneCelbrite in Celebrite">{{ uneCelbrite.photo_celebrite }}</p>
+
     </div>
 </template>
