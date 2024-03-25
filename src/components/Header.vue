39f88aed20ef6f2bf9f2a488f2547daccd7ab03b
Wali-Youssef <117752075+Wali-Youssef@users.noreply.github.com>
2024-03-25 16:03:47 +0100

    tests

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -27,7 +27,7 @@
       <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
         <RouterLink to="/" class="block text-white hover:text-gray-300">Home</RouterLink>
         <RouterLink to="/films" class="block text-white hover:text-gray-300">Films</RouterLink>
-        <RouterLink to="/fimls/edit" class="block text-white hover:text-gray-300">Créer des films</RouterLink>
+        <RouterLink to="/films/edit" class="block text-white hover:text-gray-300">Créer des films</RouterLink>
         <RouterLink to="/login-logout" class="block text-white hover:text-gray-300">Connexion</RouterLink>
       </div>
     </div>
